6c105072e78feb3485b15550e7f71e4aba27aa39
tuukka <tuukka@localhost>
2016-04-07 21:59:42 -0500

    added the necessary configuration

--- config/web.php
+++ config/web.php
@@ -9,7 +9,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => 'ugtqpw450quatjg08q43',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
