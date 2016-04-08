e16d64c81992a5e473f4b879daea1ae8acd00d9a
tuukka <tuukka@localhost>
2016-04-07 22:41:34 -0500

    autogenerated article models

---
+++ models/Article.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "article".
+ *
+ * @property integer $id
+ * @property integer $parent
+ * @property string $title
+ * @property string $content
+ * @property string $timestamp
+ */
+class Article extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'article';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['parent'], 'integer'],
+            [['title', 'content'], 'required'],
+            [['timestamp'], 'safe'],
+            [['title', 'content'], 'string', 'max' => 255]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'parent' => 'Parent',
+            'title' => 'Title',
+            'content' => 'Content',
+            'timestamp' => 'Timestamp',
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     * @return ArticleQuery the active query used by this AR class.
+     */
+    public static function find()
+    {
+        return new ArticleQuery(get_called_class());
+    }
+}
